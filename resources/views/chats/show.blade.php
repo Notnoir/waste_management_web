cd242aec79c887980f8774092745c35572f32d41
Notnoir <riza80448@gmail.com>
2025-01-17 01:28:13 +0700

    perbaikan fitur chat

--- resources/views/chats/show.blade.php
+++ resources/views/chats/show.blade.php
@@ -1,8 +1,75 @@
 @extends('layouts.app')
 
+<style>
+    /* Seluruh elemen scrollbar */
+    ::-webkit-scrollbar {
+        width: 12px;
+        /* Lebar scrollbar untuk horizontal dan vertikal */
+        height: 12px;
+        /* Tinggi scrollbar horizontal */
+    }
+
+    /* Track atau latar belakang scrollbar */
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+        /* Warna track */
+        border-radius: 6px;
+        /* Membuat sudut track membulat */
+    }
+
+    /* Thumb atau handle scrollbar */
+    ::-webkit-scrollbar-thumb {
+        background: #888;
+        /* Warna handle */
+        border-radius: 6px;
+        /* Membuat sudut handle membulat */
+    }
+
+    /* Hover pada thumb */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+        /* Warna handle saat di-hover */
+    }
+
+    /* Jika diperlukan untuk scrollbar horizontal */
+    ::-webkit-scrollbar-horizontal {
+        height: 12px;
+        /* Tinggi scrollbar horizontal */
+    }
+
+    /* Track horizontal */
+    ::-webkit-scrollbar-track:horizontal {
+        background: #f9f9f9;
+        /* Warna track horizontal */
+    }
+
+    /* Thumb horizontal */
+    ::-webkit-scrollbar-thumb:horizontal {
+        background: #777;
+        /* Warna handle horizontal */
+    }
+
+    /* Hover untuk thumb horizontal */
+    ::-webkit-scrollbar-thumb:horizontal:hover {
+        background: #444;
+    }
+</style>
+
 @section('content')
 <div class="max-w-3xl mx-auto p-6">
-    <h1 class="text-3xl font-semibold mb-4">{{ $chat->name }}</h1>
+    <div class="flex justify-between">
+        <div class="">
+            <h1 class="text-3xl font-semibold mb-4">{{ $chat->name }}</h1>
+        </div>
+        <div class="">
+            <!-- Modal toggle -->
+            <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
+                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                type="button">
+                Tambah pengguna
+            </button>
+        </div>
+    </div>
 
     <!-- Daftar Anggota Chat -->
     <div class="mb-6">
@@ -20,7 +87,7 @@
     </div>
 
     <!-- Pesan-pesan chat -->
-    <div id="message-container" class="space-y-4 mb-6 max-h-96 sm:max-h-[900px] lg:max-h-[430px] md:max-h-[850px]"
+    <div id="message-container" class="space-y-4 px-5 mb-6 max-h-96 sm:max-h-[900px] lg:max-h-[430px] md:max-h-[850px]"
         style="overflow-y: auto;">
         @foreach ($chat->messages as $message)
         <div
@@ -65,6 +132,79 @@
     </div>
 </div>
 
+<!-- Modal Tambah Pengguna -->
+<div id="crud-modal" tabindex="-1" aria-hidden="true"
+    class="hidden fixed inset-0 z-50 flex justify-center items-center w-full h-screen bg-gray-900 bg-opacity-50">
+    <div class="relative p-4 w-full max-w-md max-h-full overflow-y-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Tambah Pengguna
+                </h3>
+                <button type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form action="{{ route('chats.addMember', $chat->id) }}" method="POST" enctype="multipart/form-data"
+                class="p-4 md:p-5">
+                @csrf
+                <div class="grid gap-4">
+                    <div class="col-span-2 sm:col-span-1">
+                        {{-- <label for="region_id"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Wilayah</label>
+                        <select id="region_id" name="region_id"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            required>
+                            @foreach(\App\Models\Region::all() as $region)
+                            <option value="{{ $region->id }}">{{ $region->name }}</option>
+                            @endforeach
+                        </select> --}}
+                        <select name="user_id"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            required>
+                            <option value="" disabled selected>Pilih Pengguna</option>
+                            @foreach($users as $user)
+                            <option value="{{ $user->id }}">{{ $user->name }} - {{ $user->role }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <button type="submit"
+                    class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mt-5 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    Tambah Anggota
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Button Refresh -->
+<button id="refreshPage" title="Refresh Page"
+    class="fixed bottom-20 right-5 p-3 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 transition duration-300"
+    onclick="refreshPage()">
+    <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+        height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4" />
+    </svg>
+</button>
+
 <!-- Button Scroll to Bottom -->
 <button id="scrollToBottom" title="Scroll to Bottom"
     class="fixed bottom-5 right-5 p-3 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 transition duration-300">
@@ -74,6 +214,7 @@
             clip-rule="evenodd" />
     </svg>
 </button>
+
 
 <script>
     document.addEventListener("DOMContentLoaded", function () {
@@ -95,5 +236,9 @@
             }
         });
     });
+
+    function refreshPage() {
+        location.reload(); // Memuat ulang halaman
+    }
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chats</title>
-    @vite('resources/css/app.css') {{-- Pastikan Anda sudah menambahkan Tailwind di project Laravel --}}
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <!-- Tambahkan jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
